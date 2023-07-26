d483d6d93e209a0ea9b2fd2d15ef1dc9504bdcd7
Stanislav Redreiev <stanislav.redreiev@gmail.com>
2023-07-26 12:51:47 +0300

    ~ add inject methods to DI containers

--- Goverla/Helpers/DIContainer.swift
+++ Goverla/Helpers/DIContainer.swift
@@ -13,6 +13,7 @@ struct AppState {
 }
 
 struct DIContainer: EnvironmentKey {
+    
     let appState: AppState
     let interactors: Interactors
     
@@ -20,11 +21,43 @@ struct DIContainer: EnvironmentKey {
         self.appState = appState
         self.interactors = interactors
     }
+    
+    static var defaultValue: Self { Self.default }
+    private static let `default` = Self(appState: AppState(), interactors: Interactors.stub)
 }
 
 extension DIContainer {
     
     struct Interactors {
         let videoListInteractor: VideoListInteractorProtocol
+        
+        static var stub: Self {
+            Interactors(videoListInteractor: StubVideoListInteractor())
+        }
+    }
+}
+
+extension EnvironmentValues {
+    var injected: DIContainer {
+        get { self[DIContainer.self] }
+        set { self[DIContainer.self] = newValue }
+    }
+}
+
+// MARK: - Injection in the view hierarchy
+
+extension View {
+    
+    func inject(_ appState: AppState,
+                _ interactors: DIContainer.Interactors) -> some View {
+        let container = DIContainer(appState: appState,
+                                    interactors: interactors)
+        return inject(container)
+    }
+    
+    func inject(_ container: DIContainer) -> some View {
+        return self
+            //.modifier(RootViewAppearance())
+            .environment(\.injected, container)
     }
 }
